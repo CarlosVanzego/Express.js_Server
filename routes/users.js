815ef4a60ad7647250e5900cd02b459b86e8d08f
Carlos Vanzego <129208650+CarlosVanzego@users.noreply.github.com>
2024-01-30 00:53:31 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,64 @@
-const express = require('express')
-const router = express.Router()
+// Importing Express framework
+const express = require('express');
+// Creating an instance of Express router
+const router = express.Router();
 
+// Route to handle GET requests to the root path "/"
 router.get('/', (req, res) => {
-    console.log(req.query.name)
-    res.send('User List')
-})
+    // Logging the value of the "name" query parameter
+    console.log(req.query.name);
+    // Sending a response with the message "User List"
+    res.send('User List');
+});
 
+// Route to handle GET requests to the "/new" path
 router.get('/new', (req, res) => {
-    res.render("users/new")
-})
+    // Rendering the "users/new" view
+    res.render("users/new");
+});
 
+// Route to handle POST requests to the root path "/"
 router.post('/', (req, res) => {
-    const isValid = false
+    // Simulating validation; in this case, validation is set to false
+    const isValid = false;
     if (isValid) {
-        users.push({ firstName: req.body.firstName })
-        res.redirect(`/users/${users.length - 1}`)
+        // Adding a new user to the users array
+        users.push({ firstName: req.body.firstName });
+        // Redirecting to the user detail page with the ID of the newly added user
+        res.redirect(`/users/${users.length - 1}`);
     } else {
-        console.log("Error")
-        res.render("users/new", { firstName: req.body.firstName })
+        // If validation fails, rendering the "users/new" view with the provided data
+        console.log("Error");
+        res.render("users/new", { firstName: req.body.firstName });
     }
-    res.send("Hi")
-})
+    // Sending a response with the message "Hi" (unreachable code)
+    res.send("Hi");
+});
 
-router
-  .route('/:id')
-  .get((req, res) => {
-    console.log(req.user)
-    res.send(`Get User With ID ${req.params.id}`)
-  })
-  .put((req, res) => {
-    res.send(`Update User With ID ${req.params.id}`)
-  })
-  .delete((req, res) => {
-    res.send(`Delete User With ID ${req.params.id}`)
-  })
+// Route to handle requests to "/:id"
+router.route('/:id')
+    // Handling GET requests to retrieve user details by ID
+    .get((req, res) => {
+        console.log(req.user);
+        res.send(`Get User With ID ${req.params.id}`);
+    })
+    // Handling PUT requests to update user details by ID
+    .put((req, res) => {
+        res.send(`Update User With ID ${req.params.id}`);
+    })
+    // Handling DELETE requests to delete user details by ID
+    .delete((req, res) => {
+        res.send(`Delete User With ID ${req.params.id}`);
+    });
 
-  const users = [{ name: "Kyle" }, { name: "Sally"}]
+// Array to store user data
+const users = [{ name: "Kyle" }, { name: "Sally"}];
+// Middleware function to retrieve user data by ID
 router.param("id", (req, res, next, id) => {
-    console.log(id)
-    req.user  = users[id]
-    next()
-})
+    console.log(id);
+    req.user  = users[id];
+    next();
+});
 
-module.exports = router
-
+// Exporting the router to be used in other parts of the application
+module.exports = router;
