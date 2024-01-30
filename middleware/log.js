62db9e46a34b4bb2183720fdbb6f4b63ed174e6a
Carlos Vanzego <129208650+CarlosVanzego@users.noreply.github.com>
2024-01-30 00:54:30 -0500

    Update log.js

--- middleware/log.js
+++ middleware/log.js
@@ -1,6 +1,10 @@
+// Middleware function to log request details
 function logger(req, res, next) {
-    console.log(req.originalUrl)
-    next()
+    // Logging the original URL of the request
+    console.log(req.originalUrl);
+    // Passing control to the next middleware function in the stack
+    next();
 }
 
-module.exports = logger
+// Exporting the logger middleware function to be used in other parts of the application
+module.exports = logger;
